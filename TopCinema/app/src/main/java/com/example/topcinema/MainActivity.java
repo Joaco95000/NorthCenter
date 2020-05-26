0a064954fde9354683d802cce7d9b77ca7508f15
Axel <axel20ayalam@gmail.com>
2020-05-26 16:57:09 -0400

    finished quienes somos

--- TopCinema/app/src/main/java/com/example/topcinema/MainActivity.java
+++ TopCinema/app/src/main/java/com/example/topcinema/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.topcinema;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        VideoView videoView = findViewById(R.id.video_view);
+        String videoPath ="android.resource://"+getPackageName()+"/"+R.raw.video;
+        Uri uri = Uri.parse(videoPath);
+        videoView.setVideoURI(uri);
+
+        MediaController mediaController = new MediaController(this);
+        videoView.setMediaController(mediaController);
+        mediaController.setAnchorView(videoView);
     }
 }
